0c2d727eff003923d95dc1acfb70d214c330ef3c
shlok-k720 <shlokkhandelwal2013@gmail.com>
2025-11-01 21:19:19 -0700

    Update LLOrientation.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/LLOrientation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/LLOrientation.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.Vision;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
-import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -19,11 +18,9 @@ public class LLOrientation extends LinearOpMode {
     public LLToolkit.ChimeraLL limelight = new LLToolkit.ChimeraLL();
     public double findAngleToRotate() {
         // return allianceColor==Consts.AllianceColor.RED? Math.atan2( (Consts.Y_Coordinate_Red_Goal - curPose.getY()), (Consts.X_Coordinate_Red_Goal - curPose.getX()) ):Math.atan2( (Consts.Y_Coordinate_Blue_Goal - curPose.getY()), (Consts.X_Coordinate_Blue_Goal - curPose.getX()) );
-        return Math.atan2( (limelight.tx), (limelight.dist) ); // Maybe? multiply tx by value and see accuracy of distance.
+        return Math.atan2( (limelight.dist), limelight.tx ); // Maybe? multiply tx by value and see accuracy of distance.
     }
-    public void rotate(double angle_Radians) {
-        follower.setPose(new Pose(curPose.getX(), curPose.getY(), angle_Radians));
-    }
+    public void rotate(double angle_Radians) {follower.setPose(new Pose(curPose.getX(), curPose.getY(), angle_Radians));}
     @Override
     public void runOpMode() {
         limelight.setDevice(hardwareMap.get(Limelight3A.class, "limelight"));
