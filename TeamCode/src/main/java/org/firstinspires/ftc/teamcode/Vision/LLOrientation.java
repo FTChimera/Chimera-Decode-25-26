7f6850d0db0ebe1ffc46d024de205beb83560421
shlok-k720 <shlokkhandelwal2013@gmail.com>
2025-11-01 21:12:29 -0700

    Updated Consts.java and LLOrientation.java
    November 1,2025
    
    Made PedroPathing + Limelight first implementation of heading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/LLOrientation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/LLOrientation.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.Vision;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,20 +12,28 @@ import org.firstinspires.ftc.teamcode.pedroAuto.Constants;
 
 @TeleOp
 public class LLOrientation extends LinearOpMode {
+    public Pose startingPose;
     public static Consts.AllianceColor allianceColor = Consts.AllianceColor.RED;
     private Follower follower = Constants.createFollower(hardwareMap);
-    Pose curPose = follower.getPose();
+    public Pose curPose = follower.getPose();
     public LLToolkit.ChimeraLL limelight = new LLToolkit.ChimeraLL();
+    public double findAngleToRotate() {
+        // return allianceColor==Consts.AllianceColor.RED? Math.atan2( (Consts.Y_Coordinate_Red_Goal - curPose.getY()), (Consts.X_Coordinate_Red_Goal - curPose.getX()) ):Math.atan2( (Consts.Y_Coordinate_Blue_Goal - curPose.getY()), (Consts.X_Coordinate_Blue_Goal - curPose.getX()) );
+        return Math.atan2( (limelight.tx), (limelight.dist) ); // Maybe? multiply tx by value and see accuracy of distance.
+    }
     public void rotate(double angle_Radians) {
         follower.setPose(new Pose(curPose.getX(), curPose.getY(), angle_Radians));
     }
     @Override
     public void runOpMode() {
         limelight.setDevice(hardwareMap.get(Limelight3A.class, "limelight"));
-        waitForStart();limelight.startLLWithPipeline(0);
+        startingPose = allianceColor==Consts.AllianceColor.RED? Consts.RED_STARTING_POSE : Consts.BLUE_STARTING_POSE;follower.setStartingPose(startingPose);follower.update();
+        waitForStart();limelight.startLLWithPipeline(0);follower.startTeleopDrive();
         while (opModeIsActive()) {
-            limelight.LLUpdate();
-
+            limelight.LLUpdate();follower.update();
+            if (gamepad2.a) {
+                rotate(findAngleToRotate());
+            }
         }
     }
 }
