2e91bd417ff7180f94929a49bf0a407c1a68da76
shlok-k720 <shlokkhandelwal2013@gmail.com>
2025-11-09 16:08:48 -0800

    Update Limelight Code
    11/9

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/LLOrientation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/LLOrientation.java
@@ -19,19 +19,26 @@ public class LLOrientation extends LinearOpMode {
     public LimelightSystem.ChimeraLL limelight = new LimelightSystem.ChimeraLL();
     public double findAngleToRotate() {
         // return allianceColor==Consts.AllianceColor.RED? Math.atan2( (Consts.Y_Coordinate_Red_Goal - curPose.getY()), (Consts.X_Coordinate_Red_Goal - curPose.getX()) ):Math.atan2( (Consts.Y_Coordinate_Blue_Goal - curPose.getY()), (Consts.X_Coordinate_Blue_Goal - curPose.getX()) );
-        return limelight.tx;
+        telemetry.addData("Tx", limelight.tx);
+        return Math.toRadians(limelight.tx);
     }
     public void rotate(double angle_Radians) {follower.setPose(new Pose(curPose.getX(), curPose.getY(), angle_Radians));}
     @Override
     public void runOpMode() {
+
+        
         limelight.setDevice(hardwareMap.get(Limelight3A.class, "limelight"));
         startingPose = allianceColor==Consts.AllianceColor.RED? Consts.RED_STARTING_POSE : Consts.BLUE_STARTING_POSE;follower.setStartingPose(startingPose);follower.update();
         waitForStart();limelight.startLLWithPipeline(allianceColor==Consts.AllianceColor.RED?4:5);follower.startTeleopDrive();
+
+
         while (opModeIsActive()) {
             limelight.LLUpdate();follower.update();
             if (gamepad2.a) {
                 rotate(findAngleToRotate());
             }
+
+
         }
     }
 }
