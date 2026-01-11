89c61a574f585133a7c59c7c47a55b07a8d43b75
shlok-k720 <shlokkhandelwal2013@gmail.com>
2026-01-10 19:11:51 -0800

    Added Pedro files and converter html

---
+++ TeamCode2AutoFiles/BLUE_AUTO.pp
@@ -0,0 +1,366 @@
+{
+  "startPoint": {
+    "x": 15.75,
+    "y": 111.27,
+    "heading": "linear",
+    "startDeg": 90,
+    "endDeg": 180,
+    "locked": false
+  },
+  "lines": [
+    {
+      "id": "line-1zcnzkv32go",
+      "name": "LAUNCH",
+      "endPoint": {
+        "x": 36,
+        "y": 108,
+        "heading": "linear",
+        "startDeg": 180,
+        "endDeg": 135
+      },
+      "controlPoints": [],
+      "color": "#B66857",
+      "locked": false,
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk92epyz-4swzn4",
+      "name": "SET1",
+      "endPoint": {
+        "x": 42,
+        "y": 84,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 135,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#BA59D5",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk92ogrg-wiri56",
+      "name": "INTAKE1",
+      "endPoint": {
+        "x": 15,
+        "y": 84,
+        "heading": "tangential",
+        "reverse": false
+      },
+      "controlPoints": [],
+      "color": "#96AA79",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk92phsq-siusz9",
+      "name": "LAUNCH",
+      "endPoint": {
+        "x": 36,
+        "y": 108,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 135
+      },
+      "controlPoints": [],
+      "color": "#6D96DD",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk92rjds-gox0v5",
+      "name": "SET2",
+      "endPoint": {
+        "x": 42,
+        "y": 60,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 135,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#AA755A",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk92tq09-xnk7n7",
+      "name": "INTAKE2",
+      "endPoint": {
+        "x": 15,
+        "y": 60,
+        "heading": "tangential",
+        "reverse": false
+      },
+      "controlPoints": [],
+      "color": "#A6B999",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk94jgg7-rblm3z",
+      "name": "EMPTY_GATE",
+      "endPoint": {
+        "x": 10,
+        "y": 72,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 90
+      },
+      "controlPoints": [
+        {
+          "x": 34,
+          "y": 72
+        },
+        {
+          "x": 24,
+          "y": 72
+        }
+      ],
+      "color": "#6699DB",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk94oi5b-cgqbjh",
+      "name": "BACK_LAUNCH",
+      "endPoint": {
+        "x": 60,
+        "y": 12,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 90,
+        "endDeg": 140
+      },
+      "controlPoints": [
+        {
+          "x": 24,
+          "y": 72
+        }
+      ],
+      "color": "#ADA698",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk9503gu-zljt26",
+      "name": "SET3",
+      "endPoint": {
+        "x": 42,
+        "y": 36,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 140,
+        "endDeg": 180
+      },
+      "controlPoints": [],
+      "color": "#66A87A",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk95fne2-19r46w",
+      "name": "INTAKE3",
+      "endPoint": {
+        "x": 15,
+        "y": 36,
+        "heading": "tangential",
+        "reverse": false
+      },
+      "controlPoints": [],
+      "color": "#B9DD9D",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk95ilq9-13fh46",
+      "name": "BACK_LAUNCH",
+      "endPoint": {
+        "x": 60,
+        "y": 12,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 180,
+        "endDeg": 140
+      },
+      "controlPoints": [],
+      "color": "#5A595D",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk95jgim-p4f69p",
+      "name": "END",
+      "endPoint": {
+        "x": 60,
+        "y": 36,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 140,
+        "endDeg": 140
+      },
+      "controlPoints": [],
+      "color": "#66C56A",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    }
+  ],
+  "shapes": [
+    {
+      "id": "triangle-1",
+      "name": "Red Goal",
+      "vertices": [
+        {
+          "x": 144,
+          "y": 70
+        },
+        {
+          "x": 144,
+          "y": 144
+        },
+        {
+          "x": 120,
+          "y": 144
+        },
+        {
+          "x": 138,
+          "y": 119
+        },
+        {
+          "x": 138,
+          "y": 70
+        }
+      ],
+      "color": "#dc2626",
+      "fillColor": "#ff6b6b"
+    },
+    {
+      "id": "triangle-2",
+      "name": "Blue Goal",
+      "vertices": [
+        {
+          "x": 6,
+          "y": 119
+        },
+        {
+          "x": 25,
+          "y": 144
+        },
+        {
+          "x": 0,
+          "y": 144
+        },
+        {
+          "x": 0,
+          "y": 70
+        },
+        {
+          "x": 7,
+          "y": 70
+        }
+      ],
+      "color": "#2563eb",
+      "fillColor": "#60a5fa"
+    }
+  ],
+  "sequence": [
+    {
+      "kind": "path",
+      "lineId": "line-1zcnzkv32go"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk92epyz-4swzn4"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk92ogrg-wiri56"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk92phsq-siusz9"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk92rjds-gox0v5"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk92tq09-xnk7n7"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk94jgg7-rblm3z"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk94oi5b-cgqbjh"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk9503gu-zljt26"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk95fne2-19r46w"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk95ilq9-13fh46"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk95jgim-p4f69p"
+    }
+  ],
+  "settings": {
+    "xVelocity": 75,
+    "yVelocity": 65,
+    "aVelocity": 3.141592653589793,
+    "kFriction": 0.1,
+    "rWidth": 16,
+    "rHeight": 16,
+    "safetyMargin": 1,
+    "maxVelocity": 40,
+    "maxAcceleration": 30,
+    "maxDeceleration": 30,
+    "fieldMap": "decode.webp",
+    "robotImage": "/robot.png",
+    "theme": "auto",
+    "showGhostPaths": false,
+    "showOnionLayers": false,
+    "onionLayerSpacing": 3,
+    "onionColor": "#dc2626",
+    "onionNextPointOnly": false
+  },
+  "version": "1.2.1",
+  "timestamp": "2026-01-11T03:08:10.531Z"
+}
