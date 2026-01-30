c9617cef817e799e3ee27e441262825bfe45473e
shlok-k720 <shlokkhandelwal2013@gmail.com>
2026-01-29 18:51:58 -0800

    Update Consts.java

--- TeamCode2/src/main/java/org/firstinspires/ftc/teamcode2/Systems/Consts.java
+++ TeamCode2/src/main/java/org/firstinspires/ftc/teamcode2/Systems/Consts.java
@@ -21,10 +21,11 @@ public class Consts {
     public static final double SLEEP_BEFORE_RESET_SERVO_POSITION = 600;
     public static final double SLEEP_BEFORE_SECOND_ITERATION = 400;
     public static final double VELOCITY_TOLERANCE = 100;
-    public static double TARGET_VELOCITY_BACK_LAUNCH_ZONE = 1200;// Set target velocity from back launch zone
-    public static double TARGET_VELOCITY_FRONT_LAUNCH_ZONE = 1000;// Set target velocity from front launch zone
-    public static double MIN_VELOCITY_BACK_LAUNCH_ZONE = 1050;// Set target velocity from back launch zone
-    public static double MIN_VELOCITY_FRONT_LAUNCH_ZONE = 890;// Set target velocity from back launch zone
+    // 550 test for now. larger amounts will break the cardboard for now
+    public static double TARGET_VELOCITY_BACK_LAUNCH_ZONE = 550;// Set target velocity from back launch zone
+    public static double TARGET_VELOCITY_FRONT_LAUNCH_ZONE = 550;// Set target velocity from front launch zone
+    public static double MIN_VELOCITY_BACK_LAUNCH_ZONE = 370;// Set target velocity from back launch zone
+    public static double MIN_VELOCITY_FRONT_LAUNCH_ZONE = 370;// Set target velocity from back launch zone
     public static double STOP_VELOCITY = 0; // Set target velocity- in RPM(e.g., 3000 RPM)
     public static int[] BALL_NUM_INTAKE_NEEDED = {3};
     public static PIDFCoefficients LaunchPIDF = new PIDFCoefficients(
