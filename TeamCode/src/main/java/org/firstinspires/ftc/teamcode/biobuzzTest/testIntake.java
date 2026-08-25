8ad4dfa6c1117677e93484352954276ec7b9cc9b
shlok-k720 <shlokkhandelwal2013@gmail.com>
2026-08-24 17:43:08 -0700

    Create testIntake.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/biobuzzTest/testIntake.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.biobuzzTest;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class testIntake extends LinearOpMode {
+
+    /**
+     * Override this method and place your code here.
+     * <p>
+     * Please do not catch {@link InterruptedException}s that are thrown in your OpMode
+     * unless you are doing it to perform some brief cleanup, in which case you must exit
+     * immediately afterward. Once the OpMode has been told to stop, your ability to
+     * control hardware will be limited.
+     *
+     * @throws InterruptedException When the OpMode is stopped while calling a method
+     *                              that can throw {@link InterruptedException}
+     */
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor intake = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        intake.setDirection(DcMotorSimple.Direction.FORWARD);
+        intake.setPower(1);
+        waitForStart();
+    }
+}
